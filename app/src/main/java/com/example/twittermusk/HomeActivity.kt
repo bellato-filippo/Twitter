ffbdb35f427355176d82d8631766567eeafa05dc
Ayoub <ayoub.outmani23@gmail.com>
2022-06-17 19:17:04 +0200

    vedi mex wa

--- app/src/main/java/com/example/twittermusk/HomeActivity.kt
+++ app/src/main/java/com/example/twittermusk/HomeActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.twittermusk.adapter.PostAdapter
-import com.example.twittermusk.data.PostSource
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +16,19 @@ class HomeActivity : AppCompatActivity() {
         val ownEmail = intent.getStringExtra("passed_mail").toString()
 
 
-        val myDataset = PostSource().loadPost(ownEmail)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = PostAdapter(this, myDataset)
-
-        recyclerView.setHasFixedSize(true)
+//        val myDataset = PostSource().loadPost(ownEmail)
+//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+//        recyclerView.adapter = PostAdapter(this, myDataset)
+//
+//        recyclerView.setHasFixedSize(true)
     }
 
     override fun onStart() {
         super.onStart()
 
+        val ownProfile: Button = findViewById(R.id.ProfileButton)
         val newPost: Button = findViewById(R.id.AddButton)
         val ownEmail = intent.getStringExtra("passed_mail").toString()
-        Log.d("mail2",ownEmail)
 
         newPost.setOnClickListener {
             val intent = Intent(this, AddPostActivity::class.java)
@@ -37,11 +36,9 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val ownProfile: Button = findViewById(R.id.ProfileButton)
 
         ownProfile.setOnClickListener {
             val intent = Intent(this, OwnProfileActivity::class.java)
-            Log.d("mail3",ownEmail)
             intent.putExtra("own_email", ownEmail)
             startActivity(intent)
         }
