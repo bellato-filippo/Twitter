94cea03f9f23319c657b91582f74aa64941f0390
Ayoub <ayoub.outmani23@gmail.com>
2022-06-17 15:40:38 +0200

    vedi mex wa

--- app/src/main/java/com/example/twittermusk/OwnProfileActivity.kt
+++ app/src/main/java/com/example/twittermusk/OwnProfileActivity.kt
@@ -1,25 +1,26 @@
 package com.example.twittermusk
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultCallback
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.squareup.picasso.Picasso
 import kotlin.math.roundToInt
 
 
 class OwnProfileActivity : AppCompatActivity() {
 
-    private var uriProfilePic: Uri? = null
     private lateinit var storage: FirebaseStorage
     private lateinit var storageReference: StorageReference
 
@@ -32,34 +33,22 @@ class OwnProfileActivity : AppCompatActivity() {
         val ownEmail = intent.getStringExtra("own_email").toString()
         val picChange: Button = findViewById(R.id.change_pic)
         val picImage: ImageView = findViewById(R.id.profile_pic)
+        val username: TextView = findViewById(R.id.Username)
 
         storage = FirebaseStorage.getInstance()
         storageReference = storage.reference
 
-        val getImage = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        username.text = "Username:\n$ownEmail"
+        loadPic(ownEmail, picImage)
 
-            uriProfilePic = it
+        val getImage = registerForActivityResult(ActivityResultContracts.GetContent()) {
 
             val source = ImageDecoder.createSource(this.contentResolver, it)
             val bitmap = ImageDecoder.decodeBitmap(source)
-            val imageHeight = bitmap.height
-            val imageWidth = bitmap.width
-            val proportion: Double
-            val metrics = resources.displayMetrics
-            val dpi = metrics.densityDpi
-            val pixels = 238 * (dpi / 160)
 
-            if (imageHeight < imageWidth) {
-                proportion = imageWidth.toDouble() / pixels.toDouble()
-                val newHeight = imageHeight / proportion.roundToInt()
-                Picasso.get().load(uriProfilePic).resize(pixels, newHeight).into(picImage)
-            } else {
-                proportion = imageHeight.toDouble() / pixels.toDouble()
-                val newWidth = (imageWidth / proportion).roundToInt()
-                Picasso.get().load(uriProfilePic).resize(newWidth, pixels).into(picImage)
-            }
+            updatePic(bitmap, picImage)
 
-            uploadImageToDB(ownEmail)
+            uploadImageToDB(ownEmail,it)
         }
 
         picChange.setOnClickListener {
@@ -69,13 +58,46 @@ class OwnProfileActivity : AppCompatActivity() {
 
     }
 
+    private fun loadPic(mail: String, picImage: ImageView) {
+        try {
+            val maxDownloadSize = 10L * 1024 * 1024
+            storageReference.child("$mail/profilePic.jpg").getBytes(maxDownloadSize).addOnCompleteListener {
+                    val bitmap = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
+                    updatePic(bitmap, picImage)
+                }
+        } catch (e: Exception) {
+            Log.d("test","c'è un'eccezione mannaggina $e")
+            return
+        }
 
-    private fun uploadImageToDB(s: String) {
-        if (uriProfilePic == null) return
+
+    }
+
+    private fun updatePic(bitmap: Bitmap, picImage: ImageView) {
+        val imageHeight = bitmap.height
+        val imageWidth = bitmap.width
+        val proportion: Double
+        val metrics = resources.displayMetrics
+        val dpi = metrics.densityDpi
+        val pixels = 238 * (dpi / 160)
+
+        if (imageHeight < imageWidth) {
+            proportion = imageWidth.toDouble() / pixels.toDouble()
+            val newHeight = imageHeight / proportion.roundToInt()
+            picImage.setImageBitmap(Bitmap.createScaledBitmap(bitmap, pixels, newHeight, false))
+        } else {
+            proportion = imageHeight.toDouble() / pixels.toDouble()
+            val newWidth = (imageWidth / proportion).roundToInt()
+            picImage.setImageBitmap(Bitmap.createScaledBitmap(bitmap, newWidth, pixels, false))
+        }
+    }
+
+
+    private fun uploadImageToDB(s: String, uriProfilePic: Uri) {
 
         val pPicRef = storageReference.child("$s/profilePic.jpg")
 
-        pPicRef.putFile(uriProfilePic!!)
+        pPicRef.putFile(uriProfilePic)
             .addOnSuccessListener {
                 Snackbar.make(
                     findViewById(android.R.id.content),
